6713cc5ba382894423fd72c9bc9c0d2bb5c54e52
karenvalencia-bit <karenochoa196@gmail.com>
2020-10-28 16:45:16 -0500

    inicio

---
+++ formularioregistro.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+<nav class="navbar navbar-expand-lg  navbar-light bg-light fixed-top">
+
+
+<a class="navbar-brand" href="listado.php">
+<img src="https://www.flaticon.es/svg/static/icons/svg/1205/1205018.svg" width="30" height="30" alt="imgicono" loading="lazy">
+DULCE TENTACION
+
+  </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="listado.php">inicio <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="compra.php">compras<span class="sr-only">(current)</span></a>
+        </li>
+      </ul>
+      
+      
+    </div>
+  </nav>
+</header>
+<main>
+
+</main>
+<footer>
+
+</footer>    
+</body>
+</html>
